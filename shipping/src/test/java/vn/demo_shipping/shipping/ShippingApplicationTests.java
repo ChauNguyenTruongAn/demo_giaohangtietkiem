a788fa751eeedf93f9f2eaacf10cb464d3152a7a
An Chau <anchau03102003@gmail.com>
2025-02-26 12:24:00 +0700

    init project

---
+++ shipping/src/test/java/vn/demo_shipping/shipping/ShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.demo_shipping.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
